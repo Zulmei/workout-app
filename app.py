90d97fa751b04c1c75cd464e0db93e6ba58e3c74
Zulmei <zulmei5.eshaqzaie@gmail.com>
2024-12-02 20:39:04 -0500

    Updated welcome page styling to adjust image and layout for better UX

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import os
 import calendar
 
 # Initialize Flask app
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', static_url_path='/static')
 app.secret_key = 'your_secret_key'
 
 # Database connection function
